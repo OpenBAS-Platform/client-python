1818d5e154db89ce594fc484aac9e32a51c13d3f
Samuel Hassine <samuel.hassine@filigran.io>
2024-05-27 23:56:59 +0200

    [client] Add prevention / detection helpers

--- pyobas/apis/inject_expectation.py
+++ pyobas/apis/inject_expectation.py
@@ -22,6 +22,22 @@ class InjectExpectationManager(ListMixin, UpdateMixin, RESTManager):
         return result
 
     @exc.on_http_error(exc.OpenBASUpdateError)
+    def prevention_expectations_for_source(
+        self, source_id: str, **kwargs: Any
+    ) -> Dict[str, Any]:
+        path = f"{self.path}/prevention" + source_id
+        result = self.openbas.http_get(path, **kwargs)
+        return result
+
+    @exc.on_http_error(exc.OpenBASUpdateError)
+    def detection_expectations_for_source(
+        self, source_id: str, **kwargs: Any
+    ) -> Dict[str, Any]:
+        path = f"{self.path}/detection/" + source_id
+        result = self.openbas.http_get(path, **kwargs)
+        return result
+
+    @exc.on_http_error(exc.OpenBASUpdateError)
     def update(
         self,
         inject_expectation_id: str,
