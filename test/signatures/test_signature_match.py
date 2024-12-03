319d9893ee6937892b579bd93b1328fe056f448a
Antoine MAZEAS <antoine.mazeas@filigran.io>
2024-12-03 14:34:14 +0100

    fix test
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- test/signatures/test_signature_match.py
+++ test/signatures/test_signature_match.py
@@ -16,9 +16,9 @@ class TestSignatureMatch(unittest.TestCase):
         score = None
         SignatureMatch(match_type=MatchTypes.MATCH_TYPE_SIMPLE, match_score=score)
 
-    def test_simple_match_with_0_score_does_not_throw(self):
+    def test_fuzzy_match_with_0_score_does_not_throw(self):
         score = 0
-        SignatureMatch(match_type=MatchTypes.MATCH_TYPE_SIMPLE, match_score=score)
+        SignatureMatch(match_type=MatchTypes.MATCH_TYPE_FUZZY, match_score=score)
 
 
 if __name__ == "__main__":
