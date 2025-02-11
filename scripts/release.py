4741a8dcd85a247e1fb903c1ec95ed65c067048d
Gael Leblan <gael.leblan@filigran.io>
2025-02-11 10:32:54 +0100

    New release workflow

--- scripts/release.py
+++ scripts/release.py
@@ -1,4 +1,3 @@
-
 import argparse
 import logging
 import os
@@ -10,10 +9,18 @@ from OBAS_utils.release_utils import closeRelease
 logging.basicConfig(encoding="utf-8", level=logging.INFO)
 
 parser = argparse.ArgumentParser("release")
-parser.add_argument("branch_client_python", help="The new version number of the release.", type=str)
-parser.add_argument("previous_version", help="The previous version number of the release.", type=str)
-parser.add_argument("new_version", help="The new version number of the release.", type=str)
-parser.add_argument("--dev", help="Flag to prevent pushing the release.", action="store_false")
+parser.add_argument(
+    "branch_client_python", help="The new version number of the release.", type=str
+)
+parser.add_argument(
+    "previous_version", help="The previous version number of the release.", type=str
+)
+parser.add_argument(
+    "new_version", help="The new version number of the release.", type=str
+)
+parser.add_argument(
+    "--dev", help="Flag to prevent pushing the release.", action="store_false"
+)
 args = parser.parse_args()
 
 previous_version = args.previous_version
@@ -43,25 +50,14 @@ with open("./pyobas/_version.py", "w") as file:
 
 # Commit the change
 logging.info("[client-python] Pushing to " + branch_client_python)
-os.system(
-    'git commit -a -m "[client] Release '
-    + new_version
-    + '" > /dev/null 2>&1')
-if not args.dev :
-    os.system('git push origin '
-        + branch_client_python
-        + " > /dev/null 2>&1"
-    )
+os.system('git commit -a -m "[client] Release ' + new_version + '" > /dev/null 2>&1')
+if not args.dev:
+    os.system("git push origin " + branch_client_python + " > /dev/null 2>&1")
 
 logging.info("[client-python] Tagging")
-os.system(
-    "git tag -f "
-    + new_version
-)
-if not args.dev :
-    os.system(
-        "git push -f --tags > /dev/null 2>&1"
-    )
+os.system("git tag -f " + new_version)
+if not args.dev:
+    os.system("git push -f --tags > /dev/null 2>&1")
 
 logging.info("[client-python] Generating release")
 os.system("gren release > /dev/null 2>&1")
@@ -80,7 +76,7 @@ release_data = release.json()
 release_body = release_data["body"]
 
 logging.info("[client-python] Generating the new release note")
-if not args.dev :
+if not args.dev:
     github_release_note = requests.post(
         "https://api.github.com/repos/OpenBAS-Platform/client-python/releases/generate-notes",
         headers={
@@ -115,12 +111,16 @@ if not args.dev :
         json={"body": new_release_note},
     )
 
-if not args.dev :
-    closeRelease("https://api.github.com/repos/OpenBAS-Platform/client-python", new_version, github_token)
+if not args.dev:
+    closeRelease(
+        "https://api.github.com/repos/OpenBAS-Platform/client-python",
+        new_version,
+        github_token,
+    )
 
 logging.info(
     "[client-python] Release done! Waiting 10 minutes for CI/CD and publication..."
 )
 
-if not args.dev :
+if not args.dev:
     time.sleep(600)
