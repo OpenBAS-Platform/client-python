93534c6e1c024c771d1a98174787435599e22827
Samuel Hassine <samuel.hassine@filigran.io>
2025-02-24 18:34:40 +0100

    [client] Implement output contracts and findings (#86)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Romuald Lemesle <romuald.lemesle@filigran.io>

--- pyobas/contracts/contract_builder.py
+++ pyobas/contracts/contract_builder.py
@@ -1,16 +1,22 @@
 from typing import List
 
-from pyobas.contracts.contract_config import ContractElement
+from pyobas.contracts.contract_config import ContractElement, ContractOutputElement
 
 
 class ContractBuilder:
     fields: List[ContractElement]
+    outputs: List[ContractOutputElement]
 
     def __init__(self):
         self.fields = []
+        self.outputs = []
 
     def add_fields(self, fields: List[ContractElement]):
         self.fields = self.fields + fields
+        return self
+
+    def add_outputs(self, outputs: List[ContractOutputElement]):
+        self.outputs = self.outputs + outputs
         return self
 
     def mandatory(self, element: ContractElement):
@@ -31,5 +37,8 @@ class ContractBuilder:
             self.fields.append(element)
         return self
 
-    def build(self) -> List[ContractElement]:
+    def build_fields(self) -> List[ContractElement]:
         return self.fields
+
+    def build_outputs(self) -> List[ContractOutputElement]:
+        return self.outputs
