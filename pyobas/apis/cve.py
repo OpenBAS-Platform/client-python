3b8fc3c6c6ae84aba3537ef5cc9c8eb312702adf
MarineLM <marine.lemezo@filigran.io>
2025-07-31 17:45:53 +0200

    [client] apply black

--- pyobas/apis/cve.py
+++ pyobas/apis/cve.py
@@ -5,13 +5,14 @@ from pyobas.base import RESTManager, RESTObject
 
 
 class Cve(RESTObject):
-    _id_attr="cve_id"
+    _id_attr = "cve_id"
+
 
 class CveManager(RESTManager):
     _path = "/cves"
 
     @exc.on_http_error(exc.OpenBASUpdateError)
-    def upsert(self, data: Dict[str, Any], **kwargs: Any)-> Dict[str, Any]:
+    def upsert(self, data: Dict[str, Any], **kwargs: Any) -> Dict[str, Any]:
         path = f"{self.path}/bulk"
         result = self.openbas.http_post(path, post_data=data, **kwargs)
         return result
