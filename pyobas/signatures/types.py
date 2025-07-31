a0baa8aca850f38abcabb841a0fabb8f8abec0b6
MarineLM <marine.lemezo@filigran.io>
2025-07-31 17:36:59 +0200

    [client] add start date and end date signatures

--- pyobas/signatures/types.py
+++ pyobas/signatures/types.py
@@ -13,3 +13,5 @@ class SignatureTypes(str, Enum):
     SIG_TYPE_TARGET_IPV4_ADDRESS = "target_ipv4_address"
     SIG_TYPE_TARGET_IPV6_ADDRESS = "target_ipv6_address"
     SIG_TYPE_TARGET_HOSTNAME_ADDRESS = "target_hostname_address"
+    SIG_TYPE_START_DATE = "start_date"
+    SIG_TYPE_END_DATE = "end_date"
